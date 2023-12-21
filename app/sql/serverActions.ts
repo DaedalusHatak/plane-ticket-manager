df89cccf9d80feda101241c9d16a524a694865c9
Pawel Kicinski <pawelkic55@gmail.com>
2023-12-22 00:53:27 +0100

    sql actions for inserting flights

--- app/sql/serverActions.ts
+++ app/sql/serverActions.ts
@@ -4,5 +4,5 @@ import { sql } from "@vercel/postgres";
 
 export const handleInsertFlights = (plane:Plane,tickets:string) => {
     "use server"
-    sql`INSERT INTO flights (plane,tickets_name,prices) values(${plane?.name},${tickets})`;
+    sql`INSERT INTO flights (plane,tickets_name,prices) values(${plane?.name},${tickets}, '{93,61,44,50,34}')`;
   }
