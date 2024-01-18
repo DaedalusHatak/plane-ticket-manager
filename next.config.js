af48060e6a684a11c8887b7929308602a41c19bc
DaedalusHatak <pawelkic55@gmail.com>
2024-01-18 15:40:42 +0100

    fix: mongodb disable env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 env:{
-    LINK:process.env.NEXT_LINK,
-    API:process.env.NEXT_API,
-    DB:process.env.NEXT_DB,
+    // LINK:process.env.NEXT_LINK,
+    // API:process.env.NEXT_API,
+    // DB:process.env.NEXT_DB,
     SQL_URL:process.env.POSTGRES_URL,
     SQL_PRISMA_URL:process.env.POSTGRES_PRISMA_URL,
     SQL_URL_NON_POOLING:process.env.POSTGRES_URL_NON_POOLING,
