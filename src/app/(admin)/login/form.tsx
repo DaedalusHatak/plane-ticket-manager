62afe1ed069ec25ebd7c809207177fcb351ef8ea
Pawel Kicinski <pawelkic55@gmail.com>
2024-01-31 22:16:11 +0100

    refactor: form adjustment

--- src/app/(admin)/login/form.tsx
+++ src/app/(admin)/login/form.tsx
@@ -1,34 +1,45 @@
-"use client"
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { login } from './serverActions'
-import { Button, CircularProgress, InputLabel, TextField } from '@mui/material'
-import { useEffect, useState } from 'react'
-import { nextTick } from 'process'
+"use client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { login } from "./serverActions";
+import { Button, CircularProgress, InputLabel, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import { nextTick } from "process";
+import { useFormStatus } from "react-dom";
 
-export default  function Form(){
-    const [loading, setLoading] = useState(false)
-    useEffect(()=>{
-        console.log(loading)
-    },[loading])
-    const handleSubmit = async (formData:FormData) => {
-      
-       try{
-        setLoading(true)
-        const info = await login(formData);
-        console.log(info)
-       }catch(e){
-        console.log(e)
-       }finally{
-        // setLoading(false)
-       }
+export default function Form() {
+  const { pending } = useFormStatus();
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      const info = await login(formData);
+      console.log(info);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      // setLoading(false)
     }
+  };
 
-      return (
-        <form className='flex flex-col gap-5'>
-
-      <TextField id="email" name="email" type="email"  label="E-mail" variant="outlined" required/>
-      <TextField id="password" name="password" type="password" label="Password"  variant="outlined" required/>
-      <Button type='submit' formAction={handleSubmit} variant='contained'>{loading ? <CircularProgress /> : "Log in" }</Button>
-    </form>
-      )
+  return (
+    <>
+      <TextField
+        id="email"
+        name="email"
+        type="email"
+        label="E-mail"
+        variant="outlined"
+        required
+      />
+      <TextField
+        id="password"
+        name="password"
+        type="password"
+        label="Password"
+        variant="outlined"
+        required
+      />
+      <Button type="submit" formAction={handleSubmit} variant="contained">
+        {pending ? <CircularProgress color="secondary" /> : "Log in"}
+      </Button>
+    </>
+  );
 }
--- src/app/(admin)/login/page.tsx
+++ src/app/(admin)/login/page.tsx
@@ -1,14 +1,16 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import Form from "./form";
 import { cookies } from "next/headers";
-export default async function Page(){
-    const supabase = createServerComponentClient({cookies})
-    return (
-        <main className="flex items-center justify-center min-h-svh">
-          <div className="flex flex-col gap-10 justify-center items-center bg-slate-400 rounded-xl p-6 w-full max-w-[540px] h-[640px]">
-          <h1 className="text-4xl">Login</h1>
-            <Form></Form>
-          </div>
-        </main>
-    )
+export default async function Page() {
+  const supabase = createServerComponentClient({ cookies });
+  return (
+    <main className="flex items-center justify-center min-h-svh">
+      <div className="flex flex-col gap-10 justify-center items-center bg-slate-400 rounded-xl p-6 w-full max-w-[540px] h-[640px]">
+        <h1 className="text-4xl">Login</h1>
+        <form className="flex flex-col gap-5">
+          <Form></Form>
+        </form>
+      </div>
+    </main>
+  );
 }
